3de0833876db658ad905cd47fe251f379738fd8b
Vikash Kumar <vikkuvikash79097@gmail.com>
2023-06-06 16:43:53 +0530

    first commit

---
+++ src/components/BookShow.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import BookEdit from "./BookEdit";
+
+const BookShow = ({ book, onDelete, onEdit }) => {
+  const [showEdit, setShowEdit] = useState(false);
+
+  const handleDeleteClick = () => {
+    onDelete(book.id);
+  };
+
+  const handleEditClick = () => {
+    setShowEdit(!showEdit);
+  };
+
+  const handleSubmit = (id, newTitle) => {
+    setShowEdit(false);
+    onEdit(id, newTitle);
+  };
+
+  let content = <h3>{book.title}</h3>;
+  if (showEdit) {
+    content = <BookEdit book={book} onSubmit={handleSubmit} />;
+  }
+
+  return (
+    <div className="book-show">
+      <img
+        alt="books"
+        src={`https://picsum.photos/seed/${book.id}/300/200.jpg`}
+      />
+      <div>{content}</div>
+      <div className="actions">
+        <button className="edit" onClick={handleEditClick}>
+          Edit
+        </button>
+        <button className="delete" onClick={handleDeleteClick}>
+          Delete
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default BookShow;
